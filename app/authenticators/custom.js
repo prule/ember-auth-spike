79af3984fd2b5ff4ae7fc2da9a2465ead08aeead
Paul Rule <paulrule1@gmail.com>
2014-12-03 08:45:41 +1100

    initial commit

---
+++ app/authenticators/custom.js
@@ -0,0 +1,59 @@
+import Ember from 'ember';
+import Base from 'simple-auth/authenticators/base';
+
+export default Base.extend({
+  restore: function (data) {
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+      console.log('restore');
+      if (!Ember.isEmpty(properties.accessToken)) {
+        console.log('found access token '+properties.accessToken);
+        resolve(properties);
+      }
+      else {
+        console.log('no token found');
+        reject();
+      }
+    });
+  },
+  authenticate: function (options) {
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+      console.log('1');
+      FB.getLoginStatus(function (fbResponse) {
+        console.log('2');
+        console.log(fbResponse);
+
+        if (fbResponse.status === 'connected') {
+          Ember.run(function () {
+            console.log(fbResponse.authResponse.accessToken);
+
+            resolve({accessToken: fbResponse.authResponse.accessToken});
+          });
+        }
+        else if (fbResponse.status === 'not_authorized') {
+          reject();
+        }
+        else {
+          FB.login(function (fbResponse) {
+            if (fbResponse.authResponse) {
+              Ember.run(function () {
+                console.log(fbResponse.authResponse.accessToken);
+
+                resolve({accessToken: fbResponse.authResponse.accessToken});
+              });
+            }
+            else {
+              reject();
+            }
+          });
+        }
+      });
+    });
+  },
+  invalidate: function (data) {
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+      FB.logout(function (response) {
+        Ember.run(resolve);
+      });
+    });
+  }
+});
---
+++ config/environment.js
@@ -0,0 +1,58 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'auth-spike',
+    environment: environment,
+    baseURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      }
+    },
+    contentSecurityPolicy: {
+           'report-uri': "'self' http://static.ak.facebook.com https://www.facebook.com https://s-static.ak.facebook.com/connect/ http://connect.facebook.net/",
+           'default-src': "'self' http://static.ak.facebook.com https://www.facebook.com https://s-static.ak.facebook.com/connect/ http://connect.facebook.net/",
+           'script-src': "'self' 'unsafe-inline' 'unsafe-eval' use.typekit.net connect.facebook.net maps.googleapis.com maps.gstatic.com",
+
+           'font-src': "'self' http://fonts.gstatic.com http://netdna.bootstrapcdn.com/ http://fonts.googleapis.com",
+           'connect-src': "'self' http://localhost:8002 https://www.facebook.com http://connect.facebook.net/",
+           'img-src': "'self'",
+           'style-src': "'self' ",
+           'media-src': "'self'"
+         },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.baseURL = '/';
+    ENV.locationType = 'auto';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
